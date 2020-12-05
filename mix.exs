f9ad4f733f2c17d54cd4def7bb8db387e2fa24e6
Hentioe <me@bluerain.io>
2020-12-05 21:11:36 +0800

    Update version

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule AzureEx.MixProject do
   def project do
     [
       app: :azure_ex,
-      version: "0.1.0",
+      version: "0.1.0-dev",
       elixir: "~> 1.11",
       start_permanent: Mix.env() == :prod,
       deps: deps()
